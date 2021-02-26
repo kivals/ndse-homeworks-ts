f1499825e7db361e424a1d13784b4aa05093d70b
melnik <melnik@bifit.com>
2021-02-26 10:37:48 +0300

    Add connection to MongoDB

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,6 +1,17 @@
+import mongoose from 'mongoose';
 import app from './app';
 
 const PORT = process.env.APP_PORT || 8000;
 
-// Start server
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}!`));
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('Connected to MongoDD');
+    // Start server
+    app.listen(PORT, () => console.log(`Server is listening on port ${PORT}!`));
+  });
